2698183d65934aba835c4c97b035ec70b1e627fd
CesarVasquez27 <144802194+CesarVasquez27@users.noreply.github.com>
2024-10-20 14:53:19 -0400

    Add files via upload

---
+++ Proyecto1-EDD-main/Proyecto1-EDD-main/ProyectoChristianGitHub/ProyectoChristian/src/proyectochristian/Parada/ListaParada.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectochristian.Parada;
+
+/**
+ *
+ * @author Christian
+ */
+public class ListaParada {
+    private NodoParada pFirst;
+    private int size;
+
+    public ListaParada() {
+        this.pFirst = null;
+        this.size = 0;
+    }
+
+    public NodoParada getpFirst() {
+        return pFirst;
+    }
+
+    public void setpFirst(NodoParada pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    public boolean isEmpty(){
+        return pFirst==null;
+    }
+    
+    public void agregar(String nombreParada){
+        NodoParada pNew=new NodoParada(nombreParada);
+        if (this.isEmpty()){
+            this.setpFirst(pNew);
+        }else{
+            if(size==1){
+            this.pFirst.setpNext(pNew);
+            }else{
+                NodoParada aux=pFirst;
+                while(aux.getpNext()!=null){
+                    aux=aux.getpNext();
+                }
+                aux.setpNext(pNew);
+            }
+        }
+        size++;
+    
+    }
+    
+    public void print(){
+        if (!isEmpty()){
+            NodoParada aux=pFirst;
+            while(aux!=null){
+                System.out.println(aux.getNombreParada());
+                aux=aux.getpNext();}
+        }else{
+            System.out.println("Lista Vacia");
+        }}
+    
+    public NodoParada buscarParada(String nombreParada) {
+        NodoParada actual = pFirst;
+        while (actual != null) {
+            if (actual.getNombreParada().equals(nombreParada)) {
+                return actual;  // Parada encontrada
+            }
+            actual = actual.getpNext();
+        }
+        return null;  // Parada no encontrada
+    }   
+}    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
---
+++ Proyecto1-EDD-main/Proyecto1-EDD-main/ProyectoChristianGitHub/ProyectoChristian/src/proyectochristian/Sucursal/ListaConexion.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectochristian.Sucursal;
+
+/**
+ *
+ * @author Cesar Augusto
+ */
+public class ListaConexion {
+    private NodoConexion pFirst;
+
+    public ListaConexion() {
+        this.pFirst = null;
+    }
+
+    public NodoConexion getpFirst() {
+        return pFirst;
+    }
+
+    public void agregar(String origen, String destino) {
+        NodoConexion nuevaConexion = new NodoConexion(origen, destino);
+        if (pFirst == null) {
+            pFirst = nuevaConexion;
+        } else {
+            NodoConexion actual = pFirst;
+            while (actual.getpNext() != null) {
+                actual = actual.getpNext();
+            }
+            actual.setpNext(nuevaConexion);
+        }
+    }
+
+    public NodoConexion obtenerConexiones(String origen) {
+        NodoConexion actual = pFirst;
+        while (actual != null) {
+            if (actual.getOrigen().equals(origen)) {
+                return actual;
+            }
+            actual = actual.getpNext();
+        }
+        return null;
+    }
+}
+
