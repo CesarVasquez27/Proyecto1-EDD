3f937dd5108c64cfdc15e8568fa150bfd9448f16
chrisx1610 <113922540+chrisx1610@users.noreply.github.com>
2024-10-24 20:42:25 -0400

    Update GrafoMenu.java

--- ProyectoChristianGitHub/ProyectoChristian/src/proyectochristian/Parada/GrafoMenu.java
+++ ProyectoChristianGitHub/ProyectoChristian/src/proyectochristian/Parada/GrafoMenu.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
-
+package proyectochristian.Parada;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
